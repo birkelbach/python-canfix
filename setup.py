d35b5072281e67d166582573d81688935b3dc725
Phil Birkelbach <phil@petrasoft.net>
2014-08-28 11:31:28 -0500

    Initial Commit
    For older versions of this code see the CAN-FIX Utility repository.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='Python CANFIX',
+    version='0.1.0',
+    packages=['canfix'],
+    long_description=open('README').read(),
+    license='GNU General Public License Version 2', 
+)
+
