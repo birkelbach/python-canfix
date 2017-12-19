2935f61aea9f6d5fc09f23d50a28617c883f22c4
Phil Birkelbach <phil@petrasoft.net>
2017-12-19 11:16:59 -0600

    Make some changes to correlate to the 0.7 version of the specification.

--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-canfix',
-    version='0.1.0',
+    version='0.2.0',
     long_description=open('README.rst').read(),
+    description="Python CAN-FIX Library",
+    author="Phil Birkelbach",
+    author_email="phil@petrasoft.net",
     license='GNU General Public License Version 2',
     url='https://github.com/birkelbach/python-canfix',
     packages=['canfix'],
