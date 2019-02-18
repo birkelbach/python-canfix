b12909f2191cd0a60b67ccf5ef0e13788ad13f7e
Phil Birkelbach <phil@petrasoft.net>
2019-02-17 20:51:50 -0600

    Fix error where all packages weren't being included in distribution.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author_email="phil@petrasoft.net",
     license='GNU General Public License Version 2',
     url='https://github.com/birkelbach/python-canfix',
-    packages=['canfix'],
+    packages=find_packages(),
     package_data = {'canfix':['canfix.json']},
     install_requires = ['python-can',],
     test_suite = 'tests',
